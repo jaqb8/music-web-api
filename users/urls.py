2b7c5ef3ff1e2f1213ab706dc09f29e9f94b6fc4
Jakub Aniszewski <qba.anisz@gmail.com>
2021-09-10 22:12:48 +0200

    Add token to blacklist after logout

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ app_name = 'users'
 urlpatterns = [
     path('register/', views.RegisterUserView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh')
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('logout/', views.BlacklistTokenView.as_view(), name='blacklist-token')
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,26 @@
-from rest_framework import generics
+from rest_framework import generics, views, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
 from .serializers import RegisterUserSerializer
 
 
 class RegisterUserView(generics.CreateAPIView):
     """Create a new user in the database"""
     serializer_class = RegisterUserSerializer
+
+
+class BlacklistTokenView(views.APIView):
+    """Add token to the blacklist after user logout"""
+
+    @staticmethod
+    def post(request):
+        try:
+            refresh_token = request.data['refresh_token']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({'msg': 'Token blacklisted.'})
+        except TokenError as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except KeyError:
+            return Response({'error': 'Provide refresh token.'}, status=status.HTTP_400_BAD_REQUEST)
