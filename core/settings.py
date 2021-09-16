5526594ba038b76ace456126f4b93c3f8ee3fbd8
Jakub Aniszewski <qba.anisz@gmail.com>
2021-09-16 21:18:16 +0200

    Update

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xtm!@@h1-^&s)nbq+co2q@n08*z=v!dpil7+m^qc8#ci&ofn*8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
