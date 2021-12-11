87794b8e9a187e98ce11e46de68dea2a7ee741d4
aniszews <jakub.aniszewski@nokia.com>
2021-12-12 00:40:38 +0100

    Update

--- spotify_api/serializers.py
+++ spotify_api/serializers.py
@@ -2,34 +2,34 @@ from rest_framework import serializers
 
 
 class ArtistSerializer(serializers.Serializer):
-    id = serializers.CharField(max_length=255)
-    name = serializers.CharField(max_length=255)
+    artist_id = serializers.CharField(max_length=255, source='id')
+    artist_name = serializers.CharField(max_length=255, source='name')
 
 
 class ImageSerializer(serializers.Serializer):
-    height = serializers.IntegerField()
-    width = serializers.IntegerField()
-    url = serializers.URLField()
+    image_height = serializers.IntegerField(source='height')
+    image_width = serializers.IntegerField(source='width')
+    image_url = serializers.URLField(source='url')
 
 
 class SearchItemSerializer(serializers.Serializer):
-    id = serializers.CharField(max_length=255)
-    name = serializers.CharField(max_length=255)
-    artists = ArtistSerializer(many=True)
-    images = ImageSerializer(many=True)
+    item_id = serializers.CharField(max_length=255, source='id')
+    item_name = serializers.CharField(max_length=255, source='name')
+    item_artists = ArtistSerializer(many=True, source='artists')
+    item_images = ImageSerializer(many=True, source='images')
 
 
 class TrackSerializer(serializers.Serializer):
-    id = serializers.CharField(max_length=255)
-    name = serializers.CharField(max_length=255)
+    track_id = serializers.CharField(max_length=255, source='id')
+    track_name = serializers.CharField(max_length=255, source='name')
     track_number = serializers.IntegerField()
-    preview_url = serializers.CharField(max_length=255, required=False)
+    track_preview_url = serializers.CharField(max_length=255, required=False, source='preview_url')
 
 
 class AlbumSerializer(serializers.Serializer):
-    id = serializers.CharField(max_length=255)
-    name = serializers.CharField(max_length=255)
-    artists = ArtistSerializer(many=True)
-    images = ImageSerializer(many=True)
-    tracks = TrackSerializer(many=True)
-    release_date = serializers.CharField(max_length=255)
+    album_id = serializers.CharField(max_length=255, source='id')
+    album_name = serializers.CharField(max_length=255, source='name')
+    album_artists = ArtistSerializer(many=True, source='artists')
+    album_images = ImageSerializer(many=True, source='images')
+    album_tracks = TrackSerializer(many=True, source='tracks')
+    album_release_date = serializers.CharField(max_length=255, source='release_date')
